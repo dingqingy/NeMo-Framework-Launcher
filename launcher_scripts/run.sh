4524dc1ee599f7a5362a75e3e3a8c4780ffeb67c
Dingqing Yang <dingqingy@cs-cw-dfw-dc-02.cm.cluster>
2024-08-13 15:12:39 -0700

    test warmup overlapping

---
+++ launcher_scripts/run.sh
@@ -0,0 +1,4 @@
+python main.py \
+    training.model.mcore_customization_config.contiguous_micro_batch=5 \
+    training.model.global_batch_size=9 \
+    training.exp_manager.wandb_logger_kwargs.project=dingqingy_gpt3_overlap
