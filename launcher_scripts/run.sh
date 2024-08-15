8025fcecfeba103bfb6451791effab22b3293e8d
Dingqing Yang <dingqingy@login-eos02.eos.clusters.nvidia.com>
2024-08-15 11:43:30 -0700

    eos tunable+overlap tests

--- launcher_scripts/run.sh
+++ launcher_scripts/run.sh
@@ -1,4 +1,8 @@
 python main.py \
     training.model.mcore_customization_config.contiguous_micro_batch=5 \
-    training.model.global_batch_size=9 \
-    training.exp_manager.wandb_logger_kwargs.project=dingqingy_gpt3_overlap
+    training.model.global_batch_size=5 \
+    training.run.prefix=tunable_overlap \
+    ++training.model.deterministic_mode=false
+    
+    # training.exp_manager.wandb_logger_kwargs.project=dingqingy_gpt3_overlap
+    # base_result_dir
