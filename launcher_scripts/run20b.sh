4c272c83acedc05ab6443fd78aa91de7d0f5ec8b
Dingqing Yang <dingqingy@cw-dfw-cs-001-dc-02.cm.cluster>
2024-08-19 15:36:29 -0700

    cw 175b launch

--- launcher_scripts/run20b.sh
+++ launcher_scripts/run20b.sh
@@ -5,47 +5,43 @@ cd ../../megatron-lm
 MCORE_COMMIT=$(git rev-parse --short HEAD)
 cd -
 
-OUT_DIR=8_19_loss
+OUT_DIR=8_19_debug
 WB_PROJ=dingqingy_${OUR_DIR}
 
-NUM_NODES=16
+NUM_NODES=1
 NUM_GPUS=$((NUM_NODES*8))
-TP=4
-PP=8
-VP=12
+TP=2
+PP=4
+VP=11
 DP=$((NUM_GPUS/TP/PP))
 
 # N=$PP
 N=$((PP+1))
-M=${N}
-# M=${N}
+M=${N} # num of microbatch per pipeline
 
 MBS=1
 GBS=$((MBS*M*DP))
-# SEQ_LEN=4096
+SEQ_LEN=4096
 
-echo "num GPUs: $NUM_GPUS, dp $DP, N${N}M${M} GBS ${GBS}"
-
-python3 main.py \
-    training=gpt3/175b \
+echo "num GPU: $NUM_GPUS, dp size: $DP, N${N}M${M}, GBS: ${GBS}"
+python main.py \
+    training=gpt3/20b \
     base_results_dir=$(pwd)/${OUT_DIR} \
     training.run.prefix=${MCORE_COMMIT}_N${N}M${M} \
     training.trainer.num_nodes=${NUM_NODES} \
-    training.model.global_batch_size=${GBS} \
     training.model.micro_batch_size=${MBS} \
+    training.model.global_batch_size=${GBS} \
     training.model.tensor_model_parallel_size=${TP} \
     training.model.pipeline_model_parallel_size=${PP} \
-    training.model.virtual_pipeline_model_parallel_size=${VP} \
+    training.model.encoder_seq_length=${SEQ_LEN} \
     training.trainer.max_steps=100 \
-    training.run.time_limit=0:30:00 \
+    training.run.time_limit=0:20:00 \
     training.exp_manager.create_wandb_logger=True \
     training.exp_manager.wandb_logger_kwargs.project=${WB_PROJ} \
     +training.model.optim.grad_sync_dtype=bf16 \
     ++training.model.cross_entropy_loss_fusion=true \
     ++training.model.defer_embedding_wgrad_compute=true \
-    ++training.trainer.check_val_every_n_epoch=null \
-    ++training.trainer.num_sanity_val_steps=0 \
-    ++training.model.mcore_customization_config.contiguous_micro_batch=${N}
+    ++training.model.mcore_customization_config.contiguous_micro_batch=${N} 
+    # training.exp_manager.wandb_logger_kwargs.project=dingqingy_gpt3_debug_overlap
     # ++training.model.deterministic_mode=false \
 
-# checkpoint_callback, limit_val/test_batch
