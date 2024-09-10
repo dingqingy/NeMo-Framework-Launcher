40b6bc48e1120b61d531f5e091a06e85e26b2519
Dingqing Yang <dingqingy@cw-dfw-cs-001-dc-02.cm.cluster>
2024-09-10 12:01:46 -0700

    test decoupling send&recv issuing

--- launcher_scripts/run.sh
+++ launcher_scripts/run.sh
@@ -5,14 +5,14 @@ MCORE_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 # MCORE_COMMIT=$(git rev-parse --short HEAD)
 cd -
 
-OUT_DIR=9_4_debug_pp_only_175b_based
+OUT_DIR=9_10_decouple_test
 WB_PROJ=dingqingy_${OUT_DIR}
 
 NUM_NODES=1
 NUM_GPUS=$((NUM_NODES*8))
 TP=1
 PP=8
-VP=${1}
+VP=4
 # VP=2
 DP=$((NUM_GPUS/TP/PP))
 
@@ -20,23 +20,32 @@ DP=$((NUM_GPUS/TP/PP))
 # N=9
 N=$((PP+1))
 # M=${N}
-M=$((2*N-1))
+# M=$((2*N-1))
+M=$((2*N))
 # M=17
 
 MBS=1
 GBS=$((MBS*M*DP))
-SEQ_LEN=2048
 NUM_LAYERS=$((PP*VP))
 
-MAX_STEPS=10
-MINITES=20
+# ok timing
+SEQ_LEN=4096
+HIDDEN_SIZE=6144
+NUM_HEADS=48
+# broken timing
+# SEQ_LEN=2048
+# HIDDEN_SIZE=12288
+# NUM_HEADS=96
+
+MAX_STEPS=50
+MINITES=30
 
 echo "num GPUs: $NUM_GPUS, dp $DP, N${N}M${M} GBS ${GBS}, seq len ${SEQ_LEN}"
 
 python3 main.py \
     training=gpt3/175b \
     base_results_dir=$(pwd)/${OUT_DIR} \
-    training.run.prefix=${MCORE_BRANCH}_N${N}M${M} \
+    training.run.prefix=${MCORE_BRANCH}_N${N}M${M}_seq${SEQ_LEN}_hid${HIDDEN_SIZE}_heads${NUM_HEADS} \
     training.trainer.num_nodes=${NUM_NODES} \
     training.model.global_batch_size=${GBS} \
     training.model.micro_batch_size=${MBS} \
@@ -44,6 +53,8 @@ python3 main.py \
     training.model.pipeline_model_parallel_size=${PP} \
     training.model.virtual_pipeline_model_parallel_size=${VP} \
     training.model.encoder_seq_length=${SEQ_LEN} \
+    training.model.hidden_size=${HIDDEN_SIZE} \
+    training.model.num_attention_heads=${NUM_HEADS} \
     training.model.num_layers=${NUM_LAYERS} \
     training.trainer.max_steps=${MAX_STEPS} \
     training.run.time_limit=0:${MINITES}:00 \
@@ -56,12 +67,13 @@ python3 main.py \
     training.exp_manager.wandb_logger_kwargs.project=${WB_PROJ} \
     ++training.model.ub_tp_comm_overlap=false \
     ++training.model.sequence_parallel=false \
-    +env_vars.NCCL_DEBUG=trace \
-    +env_vars.NCCL_DEBUG_SUBSYS=call \
+    training.model.nsys_profile.enabled=true \
+    training.model.nsys_profile.start_step=20 \
+    training.model.nsys_profile.end_step=22 \
+    training.model.nsys_profile.ranks=[0,$((NUM_GPUS-1))] \
     ++training.model.mcore_customization_config.num_microbatches_per_virtual_pipe=${N}
-    # training.model.nsys_profile.enabled=true \
-    # training.model.nsys_profile.start_step=50 \
-    # training.model.nsys_profile.end_step=52 \
+    # +env_vars.NCCL_DEBUG=trace \
+    # +env_vars.NCCL_DEBUG_SUBSYS=call \
     # training.model.nsys_profile.ranks=[0,16,32,48,64,80,96,112] \
     # ++training.model.deterministic_mode=false \
 
